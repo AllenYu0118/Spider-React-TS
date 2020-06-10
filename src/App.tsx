7444f2ec0be07e6680d84ce791dea462dfe26d52
yulei <yulei@addcn.com>
2020-06-10 23:52:57 +0800

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
-import React, { Component } from 'react'
-import { Form, Icon, Input, Button } from 'antd';
-import './login.css'
+import React from 'react'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import LoginPage from './Pages/Login'
 
-interface Props {
-  form: any
+export default () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={LoginPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
-class NormalLoginForm extends Component<Props> {
-  handleSubmit = (e: any) => {
-    e.preventDefault();
-    this.props.form.validateFields((err: any, values: any) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div className="login-page">
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: '请输入登陆密码' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit"> 登 陆 </Button>
-          </Form.Item>
-        </Form>
-      </div>
-    );
-  }
-}
-
-const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
-
-export default WrappedNormalLoginForm
