bd97ec23154254abce7287d05a34add3e3fd9ea8
yulei <yulei@addcn.com>
2020-06-12 00:09:47 +0800

    feat: use echarts show data

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, message } from 'antd'
+import ReactEcharts from 'echarts-for-react'
 import axios from 'axios'
 import './style.css'
 import { Redirect } from 'react-router-dom'
@@ -26,6 +27,17 @@ class Home extends Component {
             })
     }
 
+    handleCrowllerClick = () => {
+        axios.get('/api/getData')
+            .then(res => {
+                if (res.data?.data) {
+                    message.success('爬取成功')
+                } else {
+                    message.error('爬取成功')
+                }
+            })
+    }
+
     handleLogoutClick = () => {
         axios.get('/api/logout')
             .then(res => {
@@ -39,6 +51,71 @@ class Home extends Component {
             })
     }
 
+    getOptions: () => echarts.EChartOption = () => {
+        return {
+            title: {
+                text: '折线图堆叠'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name: '邮件营销',
+                    type: 'line',
+                    stack: '总量',
+                    data: [120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name: '联盟广告',
+                    type: 'line',
+                    stack: '总量',
+                    data: [220, 182, 191, 234, 290, 330, 310]
+                },
+                {
+                    name: '视频广告',
+                    type: 'line',
+                    stack: '总量',
+                    data: [150, 232, 201, 154, 190, 330, 410]
+                },
+                {
+                    name: '直接访问',
+                    type: 'line',
+                    stack: '总量',
+                    data: [320, 332, 301, 334, 390, 330, 320]
+                },
+                {
+                    name: '搜索引擎',
+                    type: 'line',
+                    stack: '总量',
+                    data: [820, 932, 901, 934, 1290, 1330, 1320]
+                }
+            ]
+        }
+    }
+
     render() {
         const { isLogin, loaded } = this.state
 
@@ -46,9 +123,14 @@ class Home extends Component {
             if (loaded) {
                 return (
                     <div className="home-page">
-                        <Button type="primary">爬取</Button>
-                        <Button type="primary">展示</Button>
-                        <Button type="primary" onClick={this.handleLogoutClick}>退出</Button>
+                        <div className="buttons">
+                            <Button type="primary" onClick={this.handleCrowllerClick}>爬取</Button>
+                            <Button type="primary">展示</Button>
+                            <Button type="primary" onClick={this.handleLogoutClick}>退出</Button>
+                        </div>
+
+
+                        <ReactEcharts option={this.getOptions()} />
                     </div>
                 )
             }
